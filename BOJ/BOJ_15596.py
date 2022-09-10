d90c0a71dfed4ea2e9f5c8ce46a0cff6715580f2
chaewu <dev.chaewu@gmail.com>
2022-09-10 23:41:54 +0900

    09.10_Solved

---
+++ BOJ/BOJ_15596.py
@@ -0,0 +1,10 @@
+# def solve(a):   # 함수 생성 예약어 def
+#     res = 0     # 결과값을 return 할 변수 생성
+#     for i in a: 
+#         res += i
+#     return res  # 결과값 반환
+
+# 모범 답안 참고 후 코드 
+
+def solve(a):
+    return sum(a) # 입력받은 모든 a의 값을 더한 후 반환
