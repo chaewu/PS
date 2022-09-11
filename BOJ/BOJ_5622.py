348268677225dd43c4a71540c292f5ffe3b19b6c
chaewu <dev.chaewu@gmail.com>
2022-09-12 00:01:51 +0900

    09.11_Solved

--- BOJ/BOJ_5622.py
+++ BOJ/BOJ_5622.py
@@ -2,9 +2,9 @@ dial = input()
 dial_list = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
 
 sec = 0
-for unit in dial_list :  
-    for i in unit:  # alpabet 리스트에서 각 요소를 꺼내서 한글자씩 분리
-        for x in dial :  # 입력받은 문자를 하나씩 분리
-            if i == x :  # 두 알파벳이 같으면
-                sec += dial_list.index(unit) +3  # time = time + index +3
+for num in dial_list :  
+    for i in num:           # dial_list에서 값을 꺼내서 한글자씩 분리
+        for j in dial :     # 입력받은 문자를 한글자씩 분리
+            if i == j :  
+                sec += dial_list.index(num) + 3   # 입력받은 문자와 dial_list의 값이 일치할때 다이얼리스트의 인덱스값 +3 만큼을 시간에 추가
 print(sec)
