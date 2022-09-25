c71e2080d18c7f983d5f66029105ec24fa64dc6d
chaewu <dev.chaewu@gmail.com>
2022-09-26 00:04:26 +0900

    🔓 :: 09.25_Solved

--- BOJ/BOJ_1193.py
+++ BOJ/BOJ_1193.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-lne = 0
+lne = 0 # 몇번째 줄인지 표시
 end = 0
 
 while n > end:
@@ -8,11 +8,11 @@ while n > end:
     end += lne
 
 dif = end - n
-if lne%2 == 0:
-    nmt = lne - dif
-    dmt = dif + 1
-else:
+if lne%2 == 0:  # 짝수번째 라인일때는 분모가 1씩 작아지고 분모가 1씩 커진다
+    nmt = lne - dif # nmt는 분자(numerator)의 약자
+    dmt = dif + 1   # dmt는 분모(denominator)의 약자
+else:   # 홀수번째 라인일때는 분자가 1씩 커지고 분모가 1씩 작아진다
     nmt = dif + 1
     dmt = lne - dif
 
-print(f"{nmt}/{lne}")
+print(f"{nmt}/{dmt}")
