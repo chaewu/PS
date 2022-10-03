54f28a6173ddf7a8bd2023001a88b8627e9bf6c0
chaewu <dev.chaewu@gmail.com>
2022-10-04 00:13:08 +0900

    🔓 :: 10.03_Solved

--- BOJ/BOJ_1978.py
+++ BOJ/BOJ_1978.py
@@ -9,7 +9,7 @@ for i in nums:
     for j in range(2, i): # 2부터 n-1까지 (1은 소수가 아님)
       if i % j == 0:
         div_num += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가 
-      if div_num == 0:
-        p_num += 1  # 나눠지는 수가 없으므로 소수
+    if div_num == 0:
+      p_num += 1  # 나눠지는 수가 없으므로 소수
 
 print(p_num)
