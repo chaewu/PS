e5baa680fa81c5735ae51c3ba490160d628fd0e3
chaewu <dev.chaewu@gmail.com>
2022-10-14 23:55:07 +0900

    🔓 :: 10.14_Solved

--- BOJ/BOJ_8437.py
+++ BOJ/BOJ_8437.py
@@ -1,6 +1,4 @@
-apple = int(input())
-dif = int(input())
+apple = int(input()) # 사과의 개수 입력
+dif = int(input()) # 두 사과의 차이 입력
 
-avg = (apple - dif) // 2
-
-print(f"{avg + dif}\n{avg}")
+print(f"{(apple+dif) // 2}\n{(apple-dif) // 2}") # (전체 + 차이값) // 2 == Klaudia가 가진 사과의 수 (전체 - 차이값) // 2 == Natalia의 사과
