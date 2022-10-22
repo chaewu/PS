54030a9bfeff031e8bda1be0b2b5b3805ba9223b
chaewu <dev.chaewu@gmail.com>
2022-10-23 00:03:21 +0900

    🔓 :: 10.22_Solved

--- BOJ/BOJ_25314.py
+++ BOJ/BOJ_25314.py
@@ -1,3 +1,5 @@
-length = int(input()) # 입력받은 문자열을 정수형으로 변환하여 길이 값 입력
-long = "long " * (length // 4)
-print(long, end=""); print("int")
+len = int(input()) # 입력받은 문자열을 정수형으로 변환하여 길이 값 입력
+long = "long " * (len//4) # 길이값을 4로 나눈 몫 만큼 long 출력
+
+print(long, end="") # print문의 줄바꿈 제거
+print("int")
