f0cba847dd2dc2a2603f12d24afdd50e53e8ce62
chaewu <dev.chaewu@gmail.com>
2022-10-24 16:51:37 +0900

    🔓 :: 10.24_Solved

---
+++ BOJ/BOJ_24309.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print((b-c) // a) # b에서 c를 뺀 값에 a를 나눈 몫 출력
---
+++ BOJ/BOJ_24568.py
@@ -0,0 +1,4 @@
+big_box = int(input())
+sml_box = int(input())
+
+print((big_box*8 + sml_box*3) - 28) # 큰 박스에 든 케잌의 수 8개 + 작은 박스에 든 케잌의 수 3개를 합해서 학생수를 빼준다
