bbc187f023105c164b6b26cd574855fb723cf437
chaewu <dev.chaewu@gmail.com>
2022-11-16 23:18:15 +0900

    🔓 :: 11.16_Solved

---
+++ BOJ/BOJ_14581.py
@@ -0,0 +1,4 @@
+# 팬들에게 둘러쌓인 모습 출력
+print(':fan::fan::fan:')
+print(':fan::'+input()+'::fan:') # 입력을 받아 팬들 사이에 출력한다. 히히 파이썬은 출력문을 입력받아서 쓸 수 있다
+print(':fan::fan::fan:')
--- BOJ/BOJ_9020.py
+++ BOJ/BOJ_9020.py
@@ -1,14 +1,13 @@
-nums = {x for x in range(2, 10_001) if x == 2 or x % 2 == 1}
+nums = {x for x in range(2, 10_001) if x == 2 or x % 2 == 1} # 2를 포함한 1 ~ 10000까지의 홀수 집합 생성 (짝수는 소수가 아님)
 
 for odd in range(3, 101, 2): 
-    nums -= {i for i in range(2 * odd, 10_001, odd) if i in nums}
-    
+  nums -= {i for i in range(2 * odd, 10_001, odd) if i in nums}
 
 t = int(input())
 for _ in range(t):
-    even = int(input())
-    half = even//2
-    for x in range(half, 1, -1):
-        if (even-x in nums) and (x in nums):
-            print(x, even-x)
-            break
+  even = int(input())
+  half = even//2
+  for x in range(half, 1, -1):
+    if (even-x in nums) and (x in nums):
+      print(x, even-x)
+      break
