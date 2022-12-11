c0f7c42696129b156101f92a281a775f6827cf6a
chaewu <dev.chaewu@gmail.com>
2022-12-11 22:49:37 +0900

    🔓 :: 12.11_Solved

---
+++ BOJ/BOJ_14470.py
@@ -0,0 +1,10 @@
+a = int(input()) # 입력 왜 이렇게 주는거지, 화가 안날 수가 없다
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+if a < 0:
+    print(abs(a)*c+d+b*e)
+else:
+    print((b-a) * e)
