91f96813aa7e5ed9cf87e21547ad48e915037974
chaewu <dev.chaewu@gmail.com>
2023-01-04 21:30:56 +0900

    🔓 :: 1.4_Solved

---
+++ BOJ/BOJ_6778.py
@@ -0,0 +1,7 @@
+ant, eye = int(input()), int(input())
+if ant >= 3 and eye <= 4:
+    print("TroyMartian")
+if ant <= 6 and eye >= 2:
+    print("VladSaturnian")
+if ant <= 2 and eye <= 3:
+    print("GraemeMercurian")
