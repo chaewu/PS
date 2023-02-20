2aa35304e277a20b315ff66117228a1e8749e4e9
chaewu <dev.chaewu@gmail.com>
2023-02-20 21:11:13 +0900

    🔓 :: 2.20_Solved

---
+++ BOJ/BOJ_2490.py
@@ -0,0 +1,13 @@
+import sys
+from math import factorial
+
+for _ in range(3) :
+  A = list(map(int, input().split()))
+
+  result = A.count(0)
+
+  if result == 0 : print("E")
+  elif result == 1 : print("A")
+  elif result == 2 : print("B")
+  elif result == 3 : print("C")
+  elif result == 4 : print("D")
