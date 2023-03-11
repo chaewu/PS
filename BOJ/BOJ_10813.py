f41d67c12dad6ea55266e00656b9ca2370068370
chaewoo1002 <chaewoo1002@gmail.com>
2023-03-12 00:28:12 +0900

    🔓 :: 3.11_Solved

--- BOJ/BOJ_10813.py
+++ BOJ/BOJ_10813.py
@@ -1,13 +1,10 @@
-import sys
+N, M = map(int, input().split())
 
-N, M = map(int, sys.stdin.readline().split())
+ball_basket = [i for i in range(1, N + 1)]
 
-a = []
-for i in range(N):
-    a.append(i+1)
+for X in range(M):
+    i, j = map(int, input().split())
+    ball_basket[i - 1], ball_basket[j - 1] = ball_basket[j - 1], ball_basket[i - 1]
 
-for _ in range(M):
-    i, j = map(int, sys.stdin.readline().split())
-    a[i-1], a[j-1] = a[j-1], a[i-1]
 
-print(*a)
+print(*ball_basket)
