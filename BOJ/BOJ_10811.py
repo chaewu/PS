fac3db787a598e8ce5eef33924898e4e1bbd6201
chaewu <dev.chaewu@gmail.com>
2023-03-13 23:56:33 +0900

    🔓 :: 3.13_Solved

--- BOJ/BOJ_10811.py
+++ BOJ/BOJ_10811.py
@@ -1,2 +1,17 @@
 N, M = map(int, input().split())
 
+ball_basket = [i for i in range(1, N + 1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    tmp_list = []
+    for x in range(i - 1, j):
+        tmp_list.append(ball_basket[x])
+    tmp_list.reverse()
+
+    change = 0
+    for y in range(i - 1, j):
+        ball_basket[y] = tmp_list[change]
+        change += 1
+
+print(*ball_basket)
