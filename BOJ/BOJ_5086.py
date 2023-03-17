a3abb4ff5cb16c6b2a750da53b594e98b8e85d9d
chaewu <dev.chaewu@gmail.com>
2023-03-17 23:59:41 +0900

    🔓 :: 3.17_Solved

---
+++ BOJ/BOJ_5086.py
@@ -0,0 +1,13 @@
+# 출력 형식
+# 각 테스트 케이스마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력한다.
+
+while true:
+  n, m = map(int, input().split())
+  if (n == 0 and m == 0):
+    break
+  if (n / m == 0):
+    print("factor")
+  elif (m / n == 0):
+    print("multiple")
+  else:
+    print("neither")
