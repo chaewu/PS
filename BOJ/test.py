19712699022542707f978a6c45c1787c128aea4a
chaewoo1002 <chaewoo1002@gmail.com>
2023-05-04 22:20:16 +0900

    🔓 :: 5.04_Solved

--- BOJ/test.py
+++ BOJ/test.py
@@ -1,3 +1,17 @@
-from functools import reduce
+t = [list(map(int, input().split())) for _ in range(7)]
+v = [[0 for _ in range(7)] for _ in range(7)]
 
-print(reduce(lambda x, y: x + y, [0, 1, 2, 3, 4]))
+def dfs(x, y, p):  
+  if x<0 or x>6 or y<0 or y>6:
+    return 0
+  if not v[x][y] and t[x][y] == p:
+    v[x][y] = t[x][y]
+    return 1+dfs(x-1,y,p)+dfs(x+1,y,p)+dfs(x,y-1,p)+dfs(x,y+1,p)
+  return 0
+
+s = 0
+for i in range(7):
+  for j in range(7):
+    if dfs(i, j, t[i][j]) >= 3:
+      s += 1      
+print(s)
