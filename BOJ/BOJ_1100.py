b5d8f3d4b0b06f5d2556ce1d6ae603e9b1376618
chaewoo1002 <chaewoo1002@gmail.com>
2023-05-17 23:57:29 +0900

    🔓 :: 5.17_Solved

---
+++ BOJ/BOJ_1100.py
@@ -0,0 +1,15 @@
+board = []
+
+for i in range(8):
+    board.append(list(map(str, input())))
+
+result = 0
+
+for i in range(8):
+    for j in range(8):
+        if (i+j) % 2 == 0:
+
+            if board[i][j] == 'F':
+                result += 1
+
+print(result)
