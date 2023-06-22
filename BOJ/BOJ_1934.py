56c4c62edada548e0da17916d5efe819bdde21dd
chaewu <dev.chaewu@gmail.com>
2023-06-22 23:42:11 +0900

    🔓 :: 6.22_Solved

--- BOJ/BOJ_1934.py
+++ BOJ/BOJ_1934.py
@@ -1,11 +1,6 @@
-a, b = 6, 8
+import math
 
-for i in range(min(a, b), 0, -1):
-    if a % i == 0 and b % i == 0:
-        print(i)
-        break
-
-for i in range(max(a, b), (a * b) + 1):
-    if i % a == 0 and i % b == 0:
-        print(i)
-        break
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(math.lcm(a, b))
